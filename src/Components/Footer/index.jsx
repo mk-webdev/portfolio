65bf3506fb5e9dc0ef75c9f88c23fb3ea7797085
Marco Kadlubski <marco.kdlbsk@mailbox.org>
2023-03-08 17:14:16 +0100

    add digitalbeacon for later use

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 const Footer = () => {
   return (
     <>
       <footer className="container flex justify-between mx-auto mb-6">
         <p className="font-serif text-xs md:text-base">no cookies :)</p>
+
         <Link to="imprint" className="font-serif text-xs md:text-base">
           Impressum
         </Link>
